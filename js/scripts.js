41b6e2981b273445112cfd394275b005da47b189
marcrleonard <marc.r.leonard@gmail.com>
2018-10-18 21:44:39 -0600

    initial hover/select functions

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+// var hover = [];
+
+
+// // function overYo(item){
+
+// //     console.log(item);
+
+// //     fill = document.getElementById(item.id).style.fill;
+// //     if (fill == 'red'){
+// //         document.getElementById(item.id).style.fill = 'blue';
+// //     }
+// //     else {
+// //         document.getElementById(item.id).style.fill = 'red';
+// //     };
+// // };
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+
+//     d3.xml("test.svg").mimeType("image/svg+xml").get(function(error, xml) {
+//     if (error) throw error;
+//         d3.select("#svgEmbed").node().appendChild(xml.documentElement);
+//     });
+
+//     // d3.selectAll("path").style("color", "green");
+
+
+//     // a = document.getElementById("alphasvg");
+
+//     // // It's important to add an load event listener to the object,
+//     // // as it will load the svg doc asynchronously
+//     // a.addEventListener("load", function () {
+
+//     //     // get the inner DOM of alpha.svg
+//     //     var svgDoc = a.contentDocument;
+//     //     // get the inner element by id
+//     // }, false);
+// });
+
