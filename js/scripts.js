f9ca6264841847ddcea7f408cc6f1d921906ca4f
marcrleonard <marc.r.leonard@gmail.com>
2018-10-18 22:01:20 -0600

    add save and link

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,52 @@
+// var button = document.getElementById("saveButton");
+
+
+// // 3. Add event handler
+// button.addEventListener("onclick", function () {
+
+// });
+
+saveButton = function () {
+    console.log('asdasd')
+    alert("did something");
+    saveSVG();
+}
+
+
+
+saveSVG = function () {
+
+    //get svg element.
+    var svg = document.getElementById("shadow");
+
+    //get svg source.
+    var serializer = new XMLSerializer();
+    var source = serializer.serializeToString(svg);
+
+    //add name spaces.
+    if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+    if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+    }
+
+    //add xml declaration
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+    //convert svg source to URI data scheme.
+    var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+
+    //set url value to a element's href attribute.
+    link = document.getElementById("link")
+    link.href = url;
+
+    link.download = 'myAwesomeSVG.svg';
+    //you can download svg file by right click menu.
+
+
+}
+
 // var hover = [];
 
 
